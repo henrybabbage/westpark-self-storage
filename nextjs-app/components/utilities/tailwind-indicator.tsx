27601420d9fc7f418e54b56f86796afcfe399ccc
Henry Babbage <hbabbage@gmail.com>
2025-01-20 16:37:09 +1300

    add useful utils

---
+++ nextjs-app/components/utilities/tailwind-indicator.tsx
@@ -0,0 +1,14 @@
+export function TailwindIndicator() {
+  // Don't show in production
+  if (process.env.NODE_ENV === "production") return null;
+  return (
+    <div className="fixed bottom-12 left-3 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+      <div className="block sm:hidden">XS</div>
+      <div className="hidden sm:block md:hidden">SM</div>
+      <div className="hidden md:block lg:hidden">MD</div>
+      <div className="hidden lg:block xl:hidden">LG</div>
+      <div className="hidden xl:block 2xl:hidden">XL</div>
+      <div className="hidden 2xl:block">2XL</div>
+    </div>
+  );
+}
