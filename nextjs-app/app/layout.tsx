8dbae3b7d4b360f121dbbf6ba2a86ae78ccc1f66
Henry Babbage <hbabbage@gmail.com>
2025-04-06 12:51:34 +0200

    chore: integrate structured data for local business schema in layout

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -7,7 +7,9 @@ import { VisualEditing } from 'next-sanity'
 import { Inter } from 'next/font/google'
 import { draftMode } from 'next/headers'
 import { Toaster } from 'sonner'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
+import type { LocalBusiness, WithContext } from 'schema-dts'
 
 import DraftModeToast from '@/app/components/DraftModeToast'
 import Footer from '@/app/components/Footer'
@@ -65,11 +67,44 @@ const inter = Inter({
 
 const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID
 
+const jsonLd: WithContext<LocalBusiness> = {
+	'@context': 'https://schema.org',
+	'@type': 'SelfStorage',
+	name: 'Westpark Self Storage',
+	description:
+		'For a safe, clean and secure way to store your goods, speak to Westpark Self Storage in Hornby, Christchurch, New Zealand.',
+	url: 'https://westparkselfstorage.co.nz',
+	address: {
+		'@type': 'PostalAddress',
+		streetAddress: '31 Connaught Drive',
+		addressLocality: 'Hornby',
+		addressRegion: 'Christchurch',
+		postalCode: '8042',
+		addressCountry: 'NZ'
+	},
+	geo: {
+		'@type': 'GeoCoordinates',
+		latitude: -43.54749,
+		longitude: 172.50659
+	},
+	openingHours: 'Mo-Su 00:00-23:59',
+	telephone: '+64274977407'
+}
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	const { isEnabled: isDraftMode } = await draftMode()
 
 	return (
 		<html lang='en' className={cn(inter.variable, slab.variable, 'bg-white text-black')}>
+			<head>
+				<Script
+					id='schema-westpark'
+					type='application/ld+json'
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify(jsonLd)
+					}}
+				/>
+			</head>
 			<body>
 				<section className='min-h-screen pt-24'>
 					{/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
